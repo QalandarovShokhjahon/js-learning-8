d69ec322b69974a1551999c44f00d0d34f5a6df9
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-11-23 15:34:06 +0500

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Method → obyektning belgilangan vazifalarini bajarish uchun mo’ljallangan funksiyadir.
+
+// Property → obyektlarning qiymatlarini saqlash uchun ishlatiladi. property bilan method farqi propertyda () quyilmaydi.
+
